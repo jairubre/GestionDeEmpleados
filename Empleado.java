00e7ed9bb0d67c97e710cdcd55c0b1474eda3a36
jairubre <jairubre@gmail.com>
2026-04-25 16:07:40 +0200

    Clase empleado

---
+++ Empleado.java
@@ -0,0 +1,52 @@
+package GestionDeEmpleados;
+
+public abstract class Empleado {
+    protected String nombre;
+    protected String apellido;
+    protected String dni;
+    protected double salario;
+
+    public Empleado(String nombre, String apellido, String dni, double salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public abstract double calcularSalario();
+    
+
+    
+}
